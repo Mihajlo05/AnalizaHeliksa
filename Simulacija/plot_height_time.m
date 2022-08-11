962951489d151a0cecfe4f35063fd30c72fb6fa8
Mihajlo05 <68965598+Mihajlo05@users.noreply.github.com>
2022-08-11 18:35:39 +0200

    w4AA

--- Simulacija/plot_height_time.m
+++ Simulacija/plot_height_time.m
@@ -3,7 +3,7 @@ function plot_height_time(data, option)
     
     for i = 1:(length(data.time)-1)/100
         d1 = data.dpls(1, i*100);
-        d2 = data.dpls(data.n_dpls, i*100);
+        d2 = data.dpls(7, i*100);
         
         Hs(i) = abs(d2.pos(3) - d1.pos(3));
     end
